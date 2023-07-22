ed70075411678475d7096e11ce4f98aade0a6f52
Israel Kilday <israel.kilday@yahoo.com.br>
2023-07-22 16:41:25 -0300

    finishing

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function animation() {
-    const animationBtn = document.querySelector("btn")
-    const animationNavMenu = document.querySelector("nav_menu")
+    const animationBtn = document.querySelector(".btn")
+    const animationNavMenu = document.querySelector(".nav_menu")
 
     animationBtn.classList.toggle("active")
     animationNavMenu.classList.toggle("active")
