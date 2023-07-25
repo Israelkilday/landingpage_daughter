c3b8d3383edb2bc225d5be7d25e5ee1482fd5947
Israel Kilday <israel.kilday@yahoo.com.br>
2023-07-25 18:58:37 -0300

    typing index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 function animation() {
-    const animationBtn = document.querySelector(".btn")
-    const animationNavMenu = document.querySelector(".nav_menu")
+    const animationBtn = document.querySelector(".btn") as HTMLButtonElement;
+    const animationNavMenu = document.querySelector(".nav_menu") as HTMLUListElement; 
 
     animationBtn.classList.toggle("active")
     animationNavMenu.classList.toggle("active")
