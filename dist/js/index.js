75104453215281654669a589a96718e96be6a252
Israel Kilday <israel.kilday@yahoo.com.br>
2023-07-25 17:39:21 -0300

    modifying project

---
+++ dist/js/index.js
@@ -0,0 +1,7 @@
+"use strict";
+function animation() {
+    const animationBtn = document.querySelector(".btn");
+    const animationNavMenu = document.querySelector(".nav_menu");
+    animationBtn.classList.toggle("active");
+    animationNavMenu.classList.toggle("active");
+}
