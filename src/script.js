434b8388e51bbe11075666246d1714e5e9462093
Israel Kilday <israel.kilday@yahoo.com.br>
2023-08-02 23:44:16 -0300

    ading files and updating project_2

--- src/script.js
+++
@@ -1,7 +0,0 @@
-function animation() {
-    const animationBtn = document.querySelector(".btn")
-    const animationNavMenu = document.querySelector(".nav_menu")
-
-    animationBtn.classList.toggle("active")
-    animationNavMenu.classList.toggle("active")
-}
