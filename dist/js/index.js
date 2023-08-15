19ac9ff4cfd4e30787d955a8e1609c73557f1a8b
Israel Kilday <israel.kilday@yahoo.com.br>
2023-08-15 20:56:11 -0300

    correcting project

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,7 +1,7 @@
 "use strict";
 function animation() {
     const animationBtn = document.querySelector(".btn");
-    const animationNavMenu = document.querySelector(".nav_menu");
+    const animationNavMenu = document.querySelector(".navegation__nav-menu");
     animationBtn.classList.toggle("active");
     animationNavMenu.classList.toggle("active");
 }
